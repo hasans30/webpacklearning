572ea36ae67587a90066974cee74f4cfc0a66999
Hasan uz zaman Sheikh <hasan@live.in>
2023-08-16 20:36:39 -0700

    added htmlwebpackplugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = async (env,options) => {
   const config = {
   entry: './src/index.js',
@@ -7,7 +9,12 @@ module.exports = async (env,options) => {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-
+  plugins: [new HtmlWebpackPlugin({
+    filename: 'taskpane.html',
+    template: 'src/index.html',
+  }
+  )],
+ 
   }
 
   return config
